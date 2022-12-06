6f1aed17ed90da9ab720acad8f2276cede214176
MoonLGH <MoonLMC1212@gmail.com>
2022-12-06 23:31:40 +0000

    Update ready-sch to output generated at 7b72c26

---
+++ index.e27c7790.js
@@ -0,0 +1,2 @@
+let date=new Date;function getNav(){document.getElementById("navbar-cta").classList.toggle("hidden")}document.getElementById("FooterYear")&&(document.getElementById("FooterYear").innerText=document.getElementById("FooterYear").innerText.replace("xxxx",date.getFullYear()));
+//# sourceMappingURL=index.e27c7790.js.map
